df062ddec29d39a93abae2b6b2e35909994ab2f3
jasonhutchens <jason@agworld.com.au>
2014-02-19 21:38:04 +0800

    initial gh-pages commit

---
+++ config.rb
@@ -0,0 +1,19 @@
+set :source, 'website'
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :build_dir, 'build'
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :asset_hash, ignore: [/js/]
+  activate :minify_css
+  activate :minify_javascript
+end
+
+ready do
+  sprockets.append_path '../src'
+end
